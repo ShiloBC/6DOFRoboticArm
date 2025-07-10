d68f6ddbb1cddd0e4933734682bf96a08155d77a
ShiloBC <shilobc@gmail.com>
2025-07-10 19:30:32 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,31 +1,50 @@
-%% MAIN - 6-DOF Robotic Arm Kinematics and Statics Symbolic Computation
+initial_TPoint = [400; 0; 50];
+target_TPoint = [0; -400; -150];
+R = [1 0 0; 0 -1 0; 0 0 -1];
 
-% Clear workspace
-clear; clc; close all;
+TPoints = get_all_TPoints(1, 40, 10, initial_TPoint, target_TPoint);
+n = size(TPoints, 2);
+CPoints = zeros(6, n);
 
-%% Symbolic joint angles and parameters
-syms theta1 theta2 theta3 theta4 theta5 theta6 r M g real
+% Get DH parameters, transformation matixes and joint limits
+[dh, joint_limits] = dh_params();
+[A_all, A_total] = compute_transform(dh);
+jointLocations_subs = get_joints_loc(A_all, A_total, sym('theta', [6,1]));
 
-% Create DH parameter matrix
-dh = dh_params();
+% Create numerical forward kinematics function
+A_total_subs = matlabFunction(A_total, 'Vars', {sym('theta', [6,1])});
 
-%% Compute forward kinematics
-[T_all, T_total] = compute_transform(dh);
-disp('Transform matrices of the arm:');
-disp(T_all);
-disp('Total transform matrix of the arm:');
-disp(T_total);
+% Initialize 3D visualization
+G = init_plotter();
 
-%% Compute Jacobian with end-effector offset (e.g. 100 mm along z_6)
-r_offset = [0; 0; r];  % in mm
-J = compute_jacobian(T_all, r_offset);
-disp('Jacobian atrix of the arm:');
-disp(J);
+% Plot the desired path line
+plot3([initial_TPoint(1) target_TPoint(1)], ...
+      [initial_TPoint(2) target_TPoint(2)], ...
+      [initial_TPoint(3) target_TPoint(3)], ...
+      'LineWidth', 2, 'Color', 'r');
 
-%% Compute static joint torques
+% Plot intermediate target points
+plot3(TPoints(1,:), TPoints(2,:), TPoints(3,:), 'y.', 'MarkerSize', 10);
 
-tourque = compute_static_torques(J, M, g);
+% Solve inverse kinematics for each point along the path
+for i = 1:n
+    T = TPoints(:, i);
+    try
+        theta = inverse_kinematics(T, R); 
+        theta = double(theta);            
+    catch
+        warning('Failed to solve IK for point %d. Using previous point if available.', i);
+        if i > 1
+            theta = CPoints(:, i-1);
+        else
+            theta = zeros(6,1);
+        end
+    end
+    CPoints(:, i) = theta;
+end
 
-%% Display torques
-disp('Joint torques (Nm) required to hold the weight at end-effector:');
-disp(tourque);
+% Animate the robot motion step by step
+dt = 0.1;
+for i = 1:n
+    redraw_arm(CPoints(:, i), dt, G, joint_limits, jointLocations_subs);
+end
